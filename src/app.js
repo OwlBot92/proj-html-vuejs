1ed5482c66d4e771acbc337eeece3e93311185e8
OwlBot92 <48355693+OwlBot92@users.noreply.github.com>
2021-05-10 12:47:06 +0200

    nav/footer structure

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const { vue } = require("laravel-mix");
 var app = new vue({
     el: "#root",
     data: {
+        //NAV content
         navList:[
             "Home",
             "Pages",
@@ -10,9 +11,27 @@ var app = new vue({
             "Features",
             "Blog",
             "Shop"
+        ],
+
+        //FOOTER content
+        //explore
+        footerExplore: [
+            "Start here",
+            "Blog",
+            "About us",
+            "Success story",
+            "Courses",
+            "Contact us"
+        ],
+        //information
+        footerInformation: [
+            "Membership",
+            "Purchase guide",
+            "Privacy policy",
+            "Terms of service"
         ]
+    
     },
     methods: {
-
     }
 });
